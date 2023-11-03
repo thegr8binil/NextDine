75cb64d59efb48bda3e8549f2f73f2d83787ec5c
Binil B <binil.b.re@gmail.com>
2023-11-03 10:43:03 +0530

    footer

--- components/footer.js
+++ components/footer.js
@@ -1,12 +1,21 @@
-import backgroundImage from './public/T.png';
 const footer = () => {
   return (
-   
-
-    <footer style={{ backgroundImage: `url(${backgroundImage.default})` }} className="bg-cover bg-no-repeat">
-        <p>Footer</p>
-    </footer>
-    
+    <div
+      className="bg-cover bg-center max-w-9xl mt-8 flex items-center justify-center"
+      style={{
+        backgroundImage: `url("t.png")`,
+      }}
+    >
+      <div className="w-screen flex items-center mt-12 justify-center flex-col gap-4">
+        <h1 className="text-2xl text-white font-semibold">Want to Try it? </h1>
+        <h1 className="text-2xl text-white font-semibold">Try our 
+7 days free demo</h1>
+        <p className="text-white">Try demo for 7 days with full features.</p>
+        <button className="text-orange-500 bg-white p-3 rounded-lg">
+          <h1>Try free demo</h1>
+          </button>
+      </div>
+    </div>
   );
 };
 export default footer;
