18da4edbf9ce54d4eec17d8139e329312ac8e1ca
thegr8binil <binil.b.re@gmail.com>
2023-11-17 17:40:26 +0530

    downloads

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,7 @@
 import "./globals.css";
 import { Poppins } from "next/font/google";
 import NavBar from "@/components/NavBar";
-import HeroSection from "@/components/HeroSection";
-import Features from "@/components/Features";
-import Cards from "@/components/Cards";
-import Footer from "@/components/Footer";
+
 
 
 const Poppin = Poppins({ subsets: ["latin"], weight: "400" });
@@ -18,14 +15,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={Poppin.className}>
-        <div className=" w-screen flex flex-col justify-center items-center">
-          <NavBar />
-          <HeroSection />
-          <Features/>
-          <Cards/>
-          <Footer/>
+      <div className=" w-screen flex flex-col justify-center items-center">
+      <NavBar />
+      </div>
+
           {children}
-        </div>
       </body>
     </html>
   );
--- app/page.js
+++ app/page.js
@@ -1,7 +1,15 @@
+import HeroSection from "@/components/HeroSection";
+import Features from "@/components/Features";
+import Cards from "@/components/Cards";
+import Footer from "@/components/Footer";
 export default function Home() {
   return (
-  <main>
-    
+  <main className=" w-screen flex flex-col justify-center items-center">
+      
+          <HeroSection />
+          <Features/>
+          <Cards/>
+          <Footer/>
   </main> 
   )
 }
